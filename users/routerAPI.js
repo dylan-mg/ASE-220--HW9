046a1a04e344e3bd090c718dd42ff5776168f4b2
Dylan <99437817+gainesd2@users.noreply.github.com>
2022-05-01 22:09:21 -0400

    Create and index now functional
    > Routers for "/" endpoint refactored to work with mongoDB
    |-- currently no input validation, may come with future update, time permitting
    |-- create.html have also been refactored to display a success message with a link back to index when a user is successfully created

--- users/routerAPI.js
+++ users/routerAPI.js
@@ -1,33 +1,87 @@
+const { MongoClient, Db } = require('mongodb');
+
+const ID = 'browserMan';
+const PASSWORD = 'TXabiw8QTNpgjExJ';
+const DATABASE = '220--assignment-9';
+const NET = 'asecourses.lzeux.mongodb.net';
+const URL = `mongodb+srv://${ID}:${PASSWORD}@${NET}/${DATABASE}?retryWrites=true&w=majority`;
+
+/**
+ * @type {Db} MongoDB database Object
+ */
+let db;
+console.log("Please Wait for MongoDB to connect");
+MongoClient.connect(URL, { useUnifiedTopology: true }, function(error, client) {
+    if (error) return console.log(error)
+    db = client.db(DATABASE);
+    try {
+        if (error) { return console.log(error) };
+        db = client.db(DATABASE);
+        db.command({ ping: 1 });
+        console.log("MongoDB Connected");
+    } catch (tryErr) {
+        console.log(tryErr);
+        console.log("Error connecting");
+    }
+});
+
 const express = require('express')
 const router = express.Router()
-const fs=require('fs')
-
+const fs = require('fs')
 
 /* API routes */
-router.get('/',(req,res)=>{
-	let users=JSON.parse(fs.readFileSync('./data/users.json','utf8'))
-	res.status(200).json(users)
+// DOOR [ / ]
+// *GET
+// returns all user info
+router.get('/', (req, res) => {
+    db.collection('users').find().toArray(function(error, users) {
+        res.status(200).json(users);
+    });
 })
-router.post('/',(req,res)=>{
-	let users=JSON.parse(fs.readFileSync('./data/users.json','utf8'))
-	users.push(req.body)
-	fs.writeFileSync('./data/users.json',JSON.stringify(users))
-	res.status(201).json(req.body)
+
+// *POST
+// adds a new entry to list of users
+router.post('/', (req, res) => {
+    // generate the user entry for logging
+    let userEntry = {
+        email: req.body.email,
+        password: req.body.password
+    };
+
+    db.collection('users').insertOne(userEntry, function(uError, uRes) { // add post to data base
+        if (uError) { return console.log(uError) } // if error, return error
+
+        console.log(uError);
+        console.log(uRes);
+
+        res.status(201).json(req.body);
+    });
 })
-router.get('/:id',(req,res)=>{
-	let users=JSON.parse(fs.readFileSync('./data/users.json','utf8'))
-	res.status(200).json(users[req.params.id])
+
+// DOOR [ /:id ]
+// *GET
+// gets info about a specific user
+router.get('/:id', (req, res) => {
+    let users = JSON.parse(fs.readFileSync('./data/users.json', 'utf8'))
+    res.status(200).json(users[req.params.id])
 })
-router.patch('/:id',(req,res)=>{
-	let users=JSON.parse(fs.readFileSync('./data/users.json','utf8'))
-	users[req.params.id]=req.body
-	fs.writeFileSync('./data/users.json',JSON.stringify(users))
-	res.status(200).json({message:'user modified'})
+
+// *PATCH
+// updates info about a specific user
+router.patch('/:id', (req, res) => {
+    let users = JSON.parse(fs.readFileSync('./data/users.json', 'utf8'))
+    users[req.params.id] = req.body
+    fs.writeFileSync('./data/users.json', JSON.stringify(users))
+    res.status(200).json({ message: 'user modified' })
 })
-router.delete('/:id',(req,res)=>{
-	let users=JSON.parse(fs.readFileSync('./data/users.json','utf8'))
-	users.splice(req.params.id, 1)
-	fs.writeFileSync('./data/users.json',JSON.stringify(users))
-	res.status(200).json({message:'user deleted'})
+
+// *DELETE
+// deletes a specific user
+router.delete('/:id', (req, res) => {
+    let users = JSON.parse(fs.readFileSync('./data/users.json', 'utf8'))
+    users.splice(req.params.id, 1)
+    fs.writeFileSync('./data/users.json', JSON.stringify(users))
+    res.status(200).json({ message: 'user deleted' })
 })
+
 module.exports = router
